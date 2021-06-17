4f545408688afe3f5cb4f25eff6d7823f612664d
Arjun Doel <arjundoel@gmail.com>
2021-06-17 14:00:04 +0100

    add boostrap

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
-import Modal from './Modal'
+// import ModalEvent from './ModalEvent'
+
 
 const Card = ({ name, date, image, id }) => {
   const [getIDs, setIDs] = useState('')
@@ -13,25 +14,19 @@ const Card = ({ name, date, image, id }) => {
   console.log(getIDs)
   console.log(isModal)
 
-
-
-
   return (
     <>
-      {!isModal ?
-        <div className="card hoverable">
-          <div className="card-image">
-            <img onClick={triggerModal} className="responsive-img" src={image} alt={name} id={id} />
-            <span className="card-title">{name}</span>
-            <a href="" className="btn-floating halfway-fab waves-effect waves-light red"><i className="far fa-heart hoverable"></i></a>
-          </div>
-          <div className="card-content">
-            <p>{date}</p>
-          </div>
+      <div className="card hoverable">
+        <div className="card-image">
+          <img onClick={triggerModal} className="responsive-img" src={image} alt={name} id={id} />
+          <span className="card-title">{name}</span>
+          <a href="" className="btn-floating halfway-fab waves-effect waves-light red"><i className="far fa-heart hoverable"></i></a>
         </div>
-        :
-        <Modal />
-      }
+        <div className="card-content">
+          <p>{date}</p>
+        </div>
+        {/* <button className="ui button">Show Modal</button> */}
+      </div>
     </>
   )
 }
---
+++ src/component/ModalEvent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+
+const ModalEvent = () => {
+  // const [open, setOpen] = React.useState(false)
+  
+  return (
+    <h1>TEST</h1>
+  )
+}
+
+export default ModalEvent
